d1e163373b78d67376fcba1a0944a77c1ab360d7
Muhammad Umer Nadeem <76922950+Umer-dotcom@users.noreply.github.com>
2022-08-02 23:22:48 +0500

    Update!
    Completed Key without audio

--- Assets/Scripts/Lock&Key/Key.cs
+++ Assets/Scripts/Lock&Key/Key.cs
@@ -5,16 +5,34 @@ using UnityEngine;
 public class Key : MonoBehaviour
 {
     [SerializeField] private Vector2 currPos;
+    [SerializeField] private float Threshold;
+    [SerializeField] private float keyMoveSpeed;
+
+    [SerializeField] private float animPlayThreshold;
 
     private void Awake()
     {
         currPos = transform.position;
+        animPlayThreshold = currPos.x - Threshold;
         this.GetComponent<LockAxis>().xAxisLimit.y = currPos.x;
         this.GetComponent<LockAxis>().xAxisLimit.x = currPos.x - 0.933f; // 0.933 factor to move key back
     }
 
     private void Update()
     {
-        transform.position = new Vector2(transform.position.x, currPos.y);
+        if(transform.position.x <= animPlayThreshold)
+        {
+            // Play Animation
+            if(transform.position.x > this.GetComponent<LockAxis>().xAxisLimit.x)
+            {
+                Debug.Log("playing animation");
+                transform.position = Vector2.Lerp(transform.position, new Vector2(this.GetComponent<LockAxis>().xAxisLimit.x, currPos.y), Time.deltaTime * keyMoveSpeed);
+            }
+            else
+            {
+                Debug.Log("I am done with animation");
+                this.GetComponent<Draggable>().enabled = false;
+            }
+        }
     }
 }
