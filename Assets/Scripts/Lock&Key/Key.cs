9a1ca500ae0345f124b06ef0f9145198e656d386
Muhammad Umer Nadeem <76922950+Umer-dotcom@users.noreply.github.com>
2022-08-02 23:46:08 +0500

    Addition
    Added Right key

--- Assets/Scripts/Lock&Key/Key.cs
+++ Assets/Scripts/Lock&Key/Key.cs
@@ -7,23 +7,50 @@ public class Key : MonoBehaviour
     [SerializeField] private Vector2 currPos;
     [SerializeField] private float Threshold;
     [SerializeField] private float keyMoveSpeed;
+    [SerializeField] private float factorToMoveKeyBack;
 
     [SerializeField] private float animPlayThreshold;
+
+    private bool rightKey;
 
     private void Awake()
     {
         currPos = transform.position;
         animPlayThreshold = currPos.x - Threshold;
         this.GetComponent<LockAxis>().xAxisLimit.y = currPos.x;
-        this.GetComponent<LockAxis>().xAxisLimit.x = currPos.x - 0.933f; // 0.933 factor to move key back
+        this.GetComponent<LockAxis>().xAxisLimit.x = currPos.x - factorToMoveKeyBack;
+
+        // For Right Key
+        if(currPos.x < currPos.x - factorToMoveKeyBack)
+        {
+            rightKey = true;
+            this.GetComponent<LockAxis>().xAxisLimit.y = currPos.x - factorToMoveKeyBack;
+            this.GetComponent<LockAxis>().xAxisLimit.x = currPos.x;
+        }
     }
 
     private void Update()
     {
-        if(transform.position.x <= animPlayThreshold)
+        if (rightKey)
+        {
+            if(transform.position.x >= animPlayThreshold)
+            {
+                if (transform.position.x < this.GetComponent<LockAxis>().xAxisLimit.y)
+                {
+                    Debug.Log("playing animation");
+                    transform.position = Vector2.Lerp(transform.position, new Vector2(this.GetComponent<LockAxis>().xAxisLimit.y, currPos.y), Time.deltaTime * keyMoveSpeed);
+                }
+                else
+                {
+                    Debug.Log("I am done with animation");
+                    this.GetComponent<Draggable>().enabled = false;
+                }
+            } 
+        }
+        if (transform.position.x <= animPlayThreshold)
         {
             // Play Animation
-            if(transform.position.x > this.GetComponent<LockAxis>().xAxisLimit.x)
+            if (transform.position.x > this.GetComponent<LockAxis>().xAxisLimit.x)
             {
                 Debug.Log("playing animation");
                 transform.position = Vector2.Lerp(transform.position, new Vector2(this.GetComponent<LockAxis>().xAxisLimit.x, currPos.y), Time.deltaTime * keyMoveSpeed);
